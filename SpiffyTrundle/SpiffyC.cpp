00fd51817ef3938996520b95d07cecd4095984aa
xSlapppz <braden.whaley7@gmail.com>
2017-01-29 12:05:06 -0500

    Hopefully fixed GetDistance

--- SpiffyTrundle/SpiffyC.cpp
+++ SpiffyTrundle/SpiffyC.cpp
@@ -43,6 +43,7 @@ float SpiffyCommon::GetDistancePos(Vec3 pos1, Vec3 pos2)
 
 auto SpiffyCommon::GetDistance(IUnit * player, IUnit * target)
 {
+	return;
 }
 
 bool SpiffyCommon::IsUnderTurret(IUnit * Source, bool CheckAllyTurrets, bool CheckEnemyTurrets)
